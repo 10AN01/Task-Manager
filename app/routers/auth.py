aa12dd9d5b9227e75735a5840b455ec89fff12a1
Aiden <a.neizer2007@gmail.com>
2026-04-02 23:46:35 +0100

    Update auth.py

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -18,6 +18,17 @@ def register_user(user:RegisterUser):
 
 @router.post("/login")
 def login_user(user:LoginUser):
-    match = False
-    locate_email(user.email)
-    password = verify_password(user.password,hashed_password)
+# Finds the column where email exist
+    user_email = locate_email(user.email)
+    if user_email:
+# Gets the password and compares
+        hashed_password = user_email[3]
+        password = verify_password(user.password,hashed_password)
+        if password:
+            return {"Successfully logged in!"}
+        else:
+            raise HTTPException(status_code=401,detail="Invalid password")
+        
+    else:
+        raise HTTPException(status_code=404,detail = "Account don't exist.")
+        
